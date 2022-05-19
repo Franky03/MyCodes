762c096c1b4bc92fc89578f7eb01ccc0618a8ee4
Frankley Kaiky <kaikym20@gmail.com>
2022-05-18 22:56:54 -0300

    Finish day 26

--- Udemy/Day26.py
+++ Udemy/Day26.py
@@ -1,3 +1,4 @@
+import pandas as pd
 numbers= [1,2,3]
 new_list=[n+1 for n in numbers]
 print(new_list)
@@ -23,3 +24,15 @@ students_score= {student:random.randint(1,10) for student in names}
 passed_students= {student: score for (student, score) in students_score.items() if score>6}
 print(passed_students)
 # new_dict= {new_key: new_value for (key, value) in dict.items()if test}
+
+student_dict={'students': ['Angela', 'James', 'Lily'], 'score': [56, 76, 98]}
+# for k, v in student_dict.items():
+#     print(v)
+
+student_dataframe= pd.DataFrame(student_dict)
+print(student_dataframe)
+for index, row in student_dataframe.iterrows():
+    if row['students'] == 'Angela':
+        print(row.score)
+    # print(row.score)
+    # print(row.students)
---
+++ Udemy/NATOAlphabet/main.py
@@ -0,0 +1,9 @@
+import pandas as pd
+#TODO 1. Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+data= pd.read_csv('./Udemy/NATOAlphabet/nato_alph.csv')
+alphabet_dict= {row.letter: row.code for (index, row) in data.iterrows()}
+#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word= str(input('Enter a word: ')).upper()
+letters= [l for l in word]
+phon_code= [alphabet_dict[letter] for letter in letters]
+print(phon_code)
