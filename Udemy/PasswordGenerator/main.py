786ae4dd07271790de672c9177cb89af0f6cc2ed
Frankley Kaiky <kaikym20@gmail.com>
2022-05-23 23:10:27 -0300

    Final Password Genaretor

--- Udemy/PasswordGenerator/main.py
+++ Udemy/PasswordGenerator/main.py
@@ -1,8 +1,10 @@
+
 from tkinter import *
 from tkinter import messagebox
 from letersandnumbers import letters, numbers, symbols
 import random
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -48,6 +50,10 @@ def save():
         website= web_input.get()
         email= email_input.get()
         password= pass_input.get()
+        new_data={website: {
+        "email": email,
+        'password': password
+    }}
 
         # messagebox.showinfo(title='Title', message='Message')
         if len(password)==0 or len(website)==0 or len(email)==0:
@@ -57,14 +63,45 @@ def save():
             is_ok= messagebox.askokcancel(title=website, message=f'These are the details entered: \nEmail: {email}\nPassword: {password}\n Is it ok to save?')
 
             if is_ok:
-                with open('./Udemy/PasswordGenerator/passwords.txt', mode='a+') as file:
-                    file.write(f'{website} | {email} | {password}\n')
+                try:
+                    with open('./Udemy/PasswordGenerator/data.json','r') as file:
+                        data= json.load(file)
+                    
+                except FileNotFoundError:
+                    with open('./Udemy/PasswordGenerator/data.json', 'w') as file:
+                        json.dump(new_data, file, indent=4)
+                else:
+                    data.update(new_data)
+
+                    with open('./Udemy/PasswordGenerator/data.json', mode='w') as file:    
+                        json.dump(data, file, indent=4)
+                        
+                finally:        
                     web_input.delete(0, END)
                     email_input.delete(0, END)
                     pass_input.delete(0, END)
+                
+
 
     except:
-        print('ERROR')
+        messagebox.showinfo(title='Oops', message='ERROR\nPlease Try Again.')
+        
+def search_web():
+    website= web_input.get()
+    try:
+        with open('./Udemy/PasswordGenerator/data.json', mode='r') as file:
+            data= json.load(file)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Oops', message='No Data File Found.')
+    else:
+        if website in data:
+            email= data[website]['email']
+            password= data[website]['password']
+            messagebox.showinfo(title=f'{website}', message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title='Oops', message=f'No details for {website} exists.')
+
+    
         
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -84,12 +121,12 @@ email_label.grid(column=0, row=2)
 pass_label= Label(text='Password:', font= ('Arial', 12))
 pass_label.grid(column=0, row=3)
 
-web_input= Entry(width=50)
-web_input.grid(row=1, column=1, columnspan=2)
+web_input= Entry(width=32)
+web_input.grid(row=1, column=1)
 web_input.focus()
 email_input= Entry(width=50)
 email_input.grid(row=2, column=1, columnspan=2)
-# email_input.insert(END, 'kaikym20@gmail.com')
+
 pass_input= Entry(width=32, show='*')
 pass_input.grid(row=3, column=1)
 
@@ -97,6 +134,8 @@ generate_btn= Button(text='Generate Password', command= generate)
 generate_btn.grid(column=2, row=3)
 add_btn= Button(text='Add', width=35, command=save)
 add_btn.grid(column=1, row=5, columnspan=2)
+search_btn= Button(text='Search', width=14, command=search_web)
+search_btn.grid(row=1,column=2)
 
 checked_state= IntVar()
 checkbutton= Checkbutton(text='Show Password', variable=checked_state, command=show_passw)
