81dbb9c1760a2cd630bdfa3706a46d5b903e1db8
Frankley Kaiky <kaikym20@gmail.com>
2022-06-28 23:24:41 -0300

    Update Spotify Project

--- Udemy/TimeMachine/main.py
+++ Udemy/TimeMachine/main.py
@@ -1,26 +1,48 @@
 from tkinter import *
 from bs4 import BeautifulSoup
 import requests
-import os
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import spotipy.util as util
+import json
 
-SPOT_CLI= "f94dfeab131a48bfa5f265be36e0c081"
+SPOT_CLI= ""
 SPOT_KEY= "" #The Key will be reseted after i commit
+SPOT_URI= ""
+
+scope= "playlist-modify-private"
+username=""
 
 def playlist():
-    try:
-        data= date.get()
-        response = requests.get(f"https://www.billboard.com/charts/hot-100/{data}/").text
-        soup= BeautifulSoup(response, 'html.parser')
-        titles= soup.find_all("h3", id="title-of-a-story", class_="a-font-primary-bold-s")
-        songs= [song.getText().replace('\n', '').replace('\t', '') for song in titles ]
-        for song in songs:
-            if song!="Producer(s):" and song!='Imprint/Promotion Label:' and song!='Songwriter(s):':
-                print(f"{song}\n")
-        
-    except:
-        print("ERROR")
+    global scope, username
+
+    data= date.get()
+    response = requests.get(f"https://www.billboard.com/charts/hot-100/{data}/").text
+    soup= BeautifulSoup(response, 'html.parser')
+    titles= soup.find_all("h3", id="title-of-a-story", class_="a-font-primary-bold-s")
+    songs= [song.getText().replace('\n', '').replace('\t', '') for song in titles ]
+    for song in songs:
+        if song!="Producer(s):" and song!='Imprint/Promotion Label:' and song!='Songwriter(s):':
+            print(f"{song}\n")
+
+    #Spotify ascess
+            
+    token= SpotifyOAuth(
+        scope=scope, 
+        username= username, 
+        client_id=SPOT_CLI, 
+        client_secret=SPOT_KEY, 
+        cache_path="./Udemy/TimeMachine/src/token.txt", 
+        redirect_uri=SPOT_URI
+        )
+
+    spotObject= spotipy.Spotify(auth_manager= token)
+    
+    user_id = spotObject.current_user()["id"]
+    print(user_id)
+
+    
+
 
 
 
@@ -36,13 +58,26 @@ canva.grid(column=1, row=0)
 
 text= Label(text="Create a Spotify Playlist", font= ('Arial', 20))
 text.grid(column=1, row=1)
+
 date_text= Label(text="The year you want travel to:\nYYYY-MM-DD", font= ('Arial', 10))
 date_text.grid(column=1, row=2)
-
 date= Entry(width=30, justify="center")
 date.grid(column=1, row=3)
 
+play_label= Label(text="Playlist's name:", font= ('Arial', 10))
+play_label.grid(column=1, row=4)
+playlist_name= Entry(width=30, justify="center")
+playlist_name.grid(column=1, row=5)
+
+
+descr_label= Label(text="Playlist's description:", font= ('Arial', 10))
+descr_label.grid(column=1, row=6)
+play_descr= Text(screen, height = 4, width = 30)
+play_descr.grid(column=1, row=7)
+
+
+
 btn= Button(text="Create", command=playlist, width=20)
-btn.grid(column=1, row=4)
+btn.grid(column=1, row=8)
 
 screen.mainloop()
