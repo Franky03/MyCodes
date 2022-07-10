9c50f7e02ea52753a3aa8ce3a76533303905f5fe
Frankley Kaiky <kaikym20@gmail.com>
2022-07-10 18:22:38 -0300

    Tinder Bot

--- Udemy/TinderBot/bot.py
+++ Udemy/TinderBot/bot.py
@@ -3,9 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
-
-print('TESTE')
-
 options= webdriver.ChromeOptions()
 # options.add_argument("--headless")
 # options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -21,9 +18,58 @@ sleep(2)
 driver.find_element(By.XPATH, '//*[@id="q1569938320"]/div/div/div[1]/div/div/div[3]/span/div[2]/button').click()
 sleep(2)
 
-driver.switch_to(driver.window_handles[1])
+facebook= driver.window_handles[1]
+print(driver.title)
+driver.switch_to.window(facebook)
 
-sleep(1)
+sleep(2)
 driver.find_element(By.XPATH, '//*[@id="email"]').send_keys('pythonaula04@gmail.com')
+sleep(1)
 driver.find_element(By.XPATH, '//*[@id="pass"]').send_keys('cemdiascode')
-driver.find_element(By.XPATH, '//*[@id="u_0_0_dj"]').click()
+sleep(1)
+driver.find_element(By.XPATH, '//*[@id="loginbutton"]').click()
+sleep(1)
+
+
+base_window = driver.window_handles[0]
+driver.switch_to.window(base_window)
+print(driver.title)
+
+sleep(5)
+permitir= driver.find_element(By.XPATH, '//*[@id="q1569938320"]/div/div/div/div/div[3]/button[1]/span')
+permitir.click()
+sleep(1)
+n_interesse= driver.find_element(By.XPATH, '//*[@id="q1569938320"]/div/div/div/div/div[3]/button[2]')
+n_interesse.click()
+sleep(1)
+aceitar= driver.find_element(By.XPATH, '//*[@id="q-996647900"]/div/div[2]/div/div/div[1]/div[1]/button')
+aceitar.click()
+
+sleep(5)
+for n in range(80):
+    sleep(2)
+    try:
+        print('called')
+        X= driver.find_element(By.XPATH, '//*[@id="q-996647900"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[5]/div/div[4]/button')
+        X.click()
+
+    except:
+        print('called2')
+        X= driver.find_element(By.XPATH, '//*[@id="q-996647900"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button')
+        X.click()
+
+    try:
+        fechar_match= driver.find_element(By.XPATH, '//*[@id="q-666276699"]/div/div/div[1]/div/div[4]/button')
+        fechar_match.click()
+        sleep(1)
+    
+    except:
+        pass
+        
+
+
+    
+    
+    
+
+
