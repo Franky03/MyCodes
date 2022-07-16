761f23f8fa39b8474b3b661a29eafd70a2cea211
Frankley Kaiky <kaikym20@gmail.com>
2022-07-16 15:01:12 -0300

    Finish day 52

--- Udemy/InstaBot/main.py
+++ Udemy/InstaBot/main.py
@@ -2,10 +2,11 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
 
 SIMILAR= 'luizalabs'
-USERNAME= 'pythonaula04'
-PASSWORD= 'cemdiascode'
+USERNAME= '1snt4_b0t'
+PASSWORD= '***'
 
 chrome_driver_path= "C:\Development\chromedriver.exe"
 
@@ -31,13 +32,25 @@ class InstaFollower:
 
     def find_followers(self):
         self.driver.get(f'https://www.instagram.com/{SIMILAR}')
-
         sleep(2)
-
         self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[1]/div/div/div/div[1]/div[1]/section/main/div/header/section/ul/li[2]/a').click()
-# self.driver.execute_script("argument[0].scrollIntoView(true);", list_of_people[-1]) Roll pagina
+# 
     def follow(self):
-        pass
+        
+        while True:
+
+            try:
+                list_people= self.driver.find_elements(By.CSS_SELECTOR, 'li button')
+                for person in list_people:
+                    if person.text=='Seguir':
+                        person.click()
+                        sleep(3)
+                print(len(list_people))
+
+                self.driver.execute_script("argument[0].scrollIntoView(true);", list_people[-1])
+            except ElementClickInterceptedException:
+                cancel= self.driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[2]/div/div[2]/div[1]/div/div[2]/div/div/div/div/div/div/div/div[2]/button[2]')
+                cancel.click()
 
 bot= InstaFollower()
 bot.login()
