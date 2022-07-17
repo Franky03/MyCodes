d0b0f038a9cf59a08ed65c33d8b4cbf38ff2cdcf
Frankley Kaiky <kaikym20@gmail.com>
2022-07-17 18:21:43 -0300

    up

--- Udemy/Flask/hello.py
+++ Udemy/Flask/hello.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Lorem</p>"
 
 app.run()
