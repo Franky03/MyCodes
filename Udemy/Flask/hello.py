a567b897836d141aa171516748e85f60453abf15
Frankley Kaiky <kaikym20@gmail.com>
2022-07-17 22:22:31 -0300

    finish day 54

--- Udemy/Flask/hello.py
+++ Udemy/Flask/hello.py
@@ -1,9 +1,41 @@
 from flask import Flask, render_template
+import time
 
-app = Flask(__name__)
+#Python Decorator Function
+
+# def decorator(function):
+#     def wrapper_function():
+#         time.sleep(2)
+#         #Do something before
+#         function()
+#         function()
+#         #Do something after
+#     return wrapper_function
+
+# @decorator
+# def say_hello():
+#     print("Hello")
+
+# say_hello()
+
+# def say_bye():
+#     print("Bye")
+
+# say_bye()
+
+app = Flask(__name__, template_folder="C:/Users/kaiky/OneDrive/Documentos/GitHub/MyCodes/Udemy/WebDevelopment/Portfolio")
 
 @app.route("/")
-def hello_world():
-    return "<p>Lorem</p>"
+def hello():
+    return "<h1>Hello</h1>"
 
-app.run()
+@app.route("/index")
+def index():
+    return render_template("test.html")
+
+@app.route("/bye")
+def say_bye():
+    return "<p>Bye</p>"
+
+if __name__== "__main__":   
+    app.run()
