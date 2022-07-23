f643608c56eac671befae08f3cb26358c21b964d
Frankley Kaiky <kaikym20@gmail.com>
2022-07-22 22:47:31 -0300

    Atualização do site com backend

--- Udemy/Flask/HorL/challeng2.py
+++ Udemy/Flask/HorL/challeng2.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from random import randint
+from flask import Flask, redirect, render_template, request
+import json
 
 app= Flask(__name__, template_folder="C:/Users/kaiky/OneDrive/Documentos/GitHub/MyCodes/Udemy/Flask/HorL")
+opt=0
+
 
 @app.route("/")
 def init():
-    return render_template("index.html")
+    print(opt)
+    return render_template("index.html", opt= opt)
+
+
+@app.route("/", methods = ['POST'])
+def test():
+    global opt
+    output= request.get_json()
+    print(output, type(output))
+
+    num= json.loads(output)
+    print(num, type(num))
+
+    opt=num['number']
+    print(opt)
+
+    return redirect(f'/{opt}')
+
+
+@app.route(f"/{opt}")
+def menor():
+    return render_template(f"maior.html")
+
 
 if __name__== "__main__":
     app.run(debug=True)
--- Udemy/Flask/HorL/javascript.js
+++ Udemy/Flask/HorL/javascript.js
@@ -2,3 +2,18 @@ function pegarTexto() {
     const input= document.querySelector(".inp-name");
     console.log(input.value);
 };
+
+function getJson() {
+    const number= document.querySelector(".inp-name").value;
+
+    const dict_values= {number}
+    const s = JSON.stringify(dict_values);
+    console.log(s);
+    window.alert(s)
+
+    $.ajax({
+        url:"/test",
+        type: "POST",
+        contentType: "application/json",
+        data: JSON.stringify(s)});
+}
