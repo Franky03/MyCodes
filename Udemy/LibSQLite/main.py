5be7680948b1a1a17ca12274fb6f4b73e96aba5d
Frankley Kaiky <kaikym20@gmail.com>
2022-08-09 21:29:34 -0300

    finish day 63(Library with SQLite)

--- Udemy/LibSQLite/main.py
+++ Udemy/LibSQLite/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
@@ -23,35 +23,59 @@ class Book(db.Model):
 db.create_all()
 
 #CRIAR UM NOVO LIVRO
-new_book= Book(id=1, title= 'Harry Potter', author= 'J.K. Rowling', rating= 9.8)
-db.session.add(new_book)
-db.session.commit()
-        
+
+# new_book= Book(id=1, title= 'Harry Potter', author= 'J.K. Rowling', rating= 9.8)
+# db.session.add(new_book)
+# db.session.commit()
+
 # db= sqlite3.connect("./Udemy/LibSQLite/books-collection.db")
 # cursor= db.cursor()
 # cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
 # cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
 # db.commit()
 
+
 @app.route("/")
 def home():
-    return render_template("index.html", books= db)
+    all_books= Book.query.all()
+    return render_template("index.html", books= all_books)
 
 @app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method=="POST":
         dados= request.form
-
-        new_book= {
-            "title": dados['title'],
-            "author": dados['autor'], 
-            "rating": dados["rating"]
-        }
-
+        #Criando um novo livro no database
+        new_book= Book(title= dados["title"], author= dados["autor"], rating= dados["rating"])
+        db.session.add(new_book)
+        db.session.commit()
         
         return redirect(url_for('home'))
 
     return render_template("add.html")
 
+@app.route("/edit", methods= ['GET', 'POST'])
+def edit():
+    if request.method== 'POST':
+        #É preciso pegar o id e o livro especifico novamente pq o está na sessão de POST
+        #O book_selected não vai funcionar aqui pq está depois do if
+
+        book_id= request.form['id']
+        book_to_update= Book.query.get(book_id)
+        book_to_update.rating= request.form['rating']
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    book_id= request.args.get('id') #Pegar o id enviado pelo url_for do index (Edit Rating)
+    book_selected= Book.query.get(book_id) #Pegar o livro selecionado
+    return render_template("edit_rating.html", book= book_selected)
+
+@app.route('/delete')
+def delete():
+    book_id= request.args.get('id')
+    book_to_delete= Book.query.get(book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
+
 if __name__== "__main__":
-    app.run()
+    app.run(debug=True)
