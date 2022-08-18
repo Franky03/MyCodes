d395d46a680db23826acc23ac3e4e8db94948394
Frankley Kaiky <kaikym20@gmail.com>
2022-08-17 22:40:14 -0300

    update api

--- Udemy/RestfulAPI/main.py
+++ Udemy/RestfulAPI/main.py
@@ -1,5 +1,5 @@
 from random import choice
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -33,10 +33,11 @@ class Cafe(db.Model):
         return dict
 
 
+
 db.create_all()
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("index.html")
 
@@ -45,11 +46,40 @@ def home():
 def random():
     all_coffes= db.session.query(Cafe).all()
     random_cafe= choice(all_coffes)
+    #Simply convert the random_cafe data record to a dictionary of key-value pairs.
     return jsonify(cafe= random_cafe.goto_dict())
     
 
+@app.route("/all")
+def all_cafes():
+    all_coffes= db.session.query(Cafe).all()
+    return jsonify(cafes=[cafe.goto_dict() for cafe in all_coffes])
 
+@app.route("/search")
+def search():
+    query_location= request.args.get("loc")
+    cafe= Cafe.query.filter_by(location= query_location).first()
+    if cafe:
+        return jsonify(cafe= cafe.goto_dict())
+    return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
 
+@app.route("/add", methods= ["POST"])
+def add():
+    new_cafe= Cafe(
+        name= request.form.get("name"),
+        map_url= request.form.get("map_url"),
+        img_url= request.form.get("img_url"),
+        location= request.form.get("location"),
+        seats= request.form.get("seats"),
+        has_toilet= bool(request.form.get("has_toilet")),
+        has_wifi= bool(request.form.get("has_wifi")),
+        has_sockets= bool(request.form.get("has_sockets")),
+        can_take_calls= bool(request.form.get("can_take_calls")),
+        coffee_price= request.form.get("coffee_price")
+        )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response= {"success": "Successfully added the new cafe."})
 ## HTTP GET - Read Record
 
 ## HTTP POST - Create Record
