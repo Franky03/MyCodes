be9b53e6e5b62a24a011ee0141931a1bfb4e9b0c
Frankley Kaiky <kaikym20@gmail.com>
2022-08-20 23:28:51 -0300

    finish login authentication

--- Udemy/Authentication/main.py
+++ Udemy/Authentication/main.py
@@ -9,6 +9,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+#A parte de configurar o login
+
+login_manager= LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id= db.Column(db.Integer, primary_key=True)
@@ -24,31 +33,72 @@ def home():
 @app.route('/register', methods= ["GET", "POST"])
 def register():
     if request.method== "POST":
+
+        # verificar se o email (user) já e
+        if User.query.filter_by(email=request.form.get('email')).first():
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for("login"))
+
+        #é preciso gerar uma senha hash para o novo usuário, passando os parametros necessários
         new_user= User(
-            email= request.form.get('name'),
-            password= request.form.get('password'),
+            email= request.form.get('email'),
+            password= generate_password_hash(password=request.form.get('password'), method="pbkdf2:sha256", salt_length=8),
             name= request.form.get('name'),
         )
+        
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('secrets', name= new_user.name))
+
+        #Login e Autenticar o novo usuário depois de adicionar ele no database
+        login_user(new_user)
+
+        return redirect(url_for('secrets'))
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods= ["GET", "POST"])
 def login():
+    if request.method=="POST":
+        email= request.form.get("email")
+        password= request.form.get("password")
+        #Procurar o usuário no banco de dados pelo email que foi passado
+        user= User.query.filter_by(email= email).first()
+
+        #User não existe
+
+        if not user: 
+            flash("That email does not exist, please try again.")
+            return redirect(url_for("login"))
+        
+        #Senha errada
+
+        # check_password_hash checa se a senha passada no login é a mesma senha hash do banco de dados
+        elif not check_password_hash(user.password, password):
+            flash("Password incorrect, please try again.")
+            return redirect(url_for("login"))
+        
+        #Email existe e a senha está correta
+
+        else:
+            #Para logar o usuário é preciso usar a função login_user
+            login_user(user=user)
+            return redirect(url_for("secrets"))
     return render_template("login.html")
 
 
-@app.route('/secrets/<name>')
-def secrets(name):
-    return render_template("secrets.html", name= name)
-
+@app.route('/secrets')
+@login_required
+#usar o decorator login_required para acessar essa rota apenas se estiver um login ativo
+def secrets():
+    #Usar current_user para pegar o usuário atual logado e usar o nome dele como também as outras informações
+    print(current_user.name)
+    return render_template("secrets.html", name= current_user.name)
 
 @app.route('/logout')
 def logout():
-    pass
-
+    #Para deslogar o usuário, pois se ele continuar logado, será possivel entrar no perfil dele
+    logout_user()
+    return redirect(url_for("home"))
 
 @app.route('/download')
 def download():
