4193e9a5ce4998071b5503b49f47ca35ded36ed0
Frankley Kaiky <kaikym20@gmail.com>
2022-10-13 12:54:05 -0300

    Revisando código

--- Udemy/RestfulAPI/main.py
+++ Udemy/RestfulAPI/main.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+#Creating an API
+
 ##Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
